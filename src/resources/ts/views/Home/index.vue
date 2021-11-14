8398e091ba59b0d4ba1630d06574948899457105
yh72167 <yh.19972372@gmail.com>
2021-11-14 19:41:46 +0900

    [wip]認証画面の作成

--- src/resources/ts/views/Home/index.vue
+++ src/resources/ts/views/Home/index.vue
@@ -1,3 +1,127 @@
 <template>
-  <div>Hello</div>
+
+  <body class="auth">
+
+    <div class="auth-wrap">
+
+      <section class="auth__">
+        <div>
+          <img class="" :src="'/img/HomeImg.svg'">
+        </div>
+      </section>
+
+      <section>
+        <div>ロゴ</div>
+        <input>
+        <input>
+        <button>ログイン</button>
+        <div>
+          <div></div>
+          または
+          <div></div>
+        </div>
+
+        <div>
+          アカウントをお持ちでないですか？<a href class="">登録する</a>
+        </div>
+
+        <div>
+          <div>ダウンロード</div>
+          <button>
+            AppStore
+          </button>
+          <button>
+            グーグルプレイで手に入れよう
+          </button>
+        </div>
+      </section>
+
+      <section>
+
+        <div>ロゴ</div>
+        <div>
+          <div>登録して友達の写真や動画をチェ</div>
+          <div>ックしよう</div>
+        </div>
+
+        <button>Facebookでログイン</button>
+
+        <div>
+          <div></div>
+          または
+          <div></div>
+        </div>
+
+        <div>
+
+          <input>
+          <input>
+          <input>
+          <input>
+
+          <button>
+          登録する
+          </button>
+
+          <div>
+            <span>登録することで、Facebookの利用規約、データ</span>
+            <span>に関するポリシー、Cookieポリシーに同意する</span>
+            <span>ものとします。</span>
+          </div>
+
+        </div>
+
+        <div>
+          <div>
+            <span>アカウントをお持ちですか？</span>
+            <a href="#">ログインする</a>
+          </div>
+        </div>
+
+        ダウンロード
+        <button>
+          AppStore
+        </button>
+        <button>
+          Google Playで手に入れよう
+        </button>
+
+      </section>
+
+      <footer>
+        <div>
+          <div>
+            <span>メタ</span>
+            <span>基本データ</span>
+            <span>ブログ</span>
+            <span>求人</span>
+            <span>ヘルプ</span>
+            <span>API</span>
+            <span>プライバシー</span>
+            <span>利用規約</span>
+            <span>人気アカウント</span>
+            <span>ハッシュタグ</span>
+            <span>所在地</span>
+            <span>Instagram Lite</span>
+          </div>
+          <div>
+            <span>美容</span>
+            <span>ダンス</span>
+            <span>フィットネス</span>
+            <span>フード・ドリンク</span>
+            <span>ホーム・ガーデン</span>
+            <span>音楽</span>
+            <span>視覚芸術</span>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+  </body>
+
 </template>
+<style lang="scss">
+  .auth{
+    background: #fff;
+  }
+</style>
